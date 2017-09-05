b2737691c208bf79320c9189c5ed2dde99457e01
Andrea Coaker <andicoaker@gmail.com>
2017-09-05 16:36:17 -0400

    used lodash _.mapKeys to complete reducer_posts

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,16 @@
+import { FETCH_POSTS } from '../actions';
+import _ from 'lodash';
+
+// set initial state to default to empty obj
+export default function (state = {}, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      console.log(action.payload.data);
+      // using lodash _.mapKeys to take an array and turn it into an object itself
+      // code below pulls off the id property from every incoming post
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
+
+  }
+}
