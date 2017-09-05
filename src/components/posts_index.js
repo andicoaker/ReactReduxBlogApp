e1b2628de5884b1f50b774e6055c9505d8809085
Andrea Coaker <andicoaker@gmail.com>
2017-09-05 17:12:29 -0400

    updating posts_index file

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+
+  // react life-cycle method - a function on a react component class that is automatically called by react.
+  // example - componentDidMount, allows you to fetch data after component renders on the screen
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
   render () {
     return (
@@ -12,4 +20,6 @@ class PostsIndex extends Component {
 
 }
 
-export default PostsIndex;
+// first argument set to null because we're not passing in map state to props.
+// code below is just like using the map dispatch to props function used in previous projects
+export default connect (null, { fetchPosts })(PostsIndex);
