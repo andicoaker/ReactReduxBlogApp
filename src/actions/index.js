ce5c0a8c12f6564c518a3a1ce8a9ee7f7c28cc44
Andrea Coaker <andicoaker@gmail.com>
2017-09-05 16:20:53 -0400

    installed axios & react-promise, created posts_index component, set-up basic react-router, set-up actions, started working on redcuer index file

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=PENZ07'
+
+// make axios request, assign to the variable request, then assign request to the payload property of the action we're requiring.
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// BrowserRouter interacts with history library.
+// Route provides configuration that says is url looks like this, show this component.
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+// we no longer need App now that we're using react-router, so we can delete this and the app.js file as well
+// import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+
+// If user goes to a route path, show the component listed
+// adding :id to the path gives react-router a wild card that it will try to match to
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
