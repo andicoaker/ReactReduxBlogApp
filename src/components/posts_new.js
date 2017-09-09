1a8fb8577056dbb3d251b820c63e7cf8641e99e3
Andrea Coaker <andicoaker@gmail.com>
2017-09-09 15:24:30 -0400

    added redux-form and formReducer to reducers/index.js; wired posts_new to reducer and started creating redux-form Field components to posts_new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
 
 class PostsNew extends Component {
-  render() {
+
+  // field argument contains event handlers needed to wire-up the jsx being returned
+  // ...field.input says this is an object and i want all the objects properties to be communicated as props to the input tag. saves us from having to do onChange, onFocuse, onBlur, etc.
+  renderTitleField(field) {
     return (
       <div>
-        PostsNew!
+        <input
+          type="text"
+          {...field.input}
+        />
       </div>
+    );
+  }
+
+  // name property specificies the piece of state this component will produce
+  // component property in the form field component adds a function that will return some jsx to show the field component on the screen
+
+  render() {
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// reduxForm function helper to wrap the PostsNew component. this allows component to talk to the associated reducer.
+// assign a unique string to the form property
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
