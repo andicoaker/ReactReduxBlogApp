3fa6783faa4b6050e151a074e7f49da56ab8e6d9
Andrea Coaker <andicoaker@gmail.com>
2017-09-09 17:06:55 -0400

    added bootstrap styling using classNames and ternary expressions

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,17 +5,24 @@ class PostsNew extends Component {
 
   // field argument contains event handlers needed to wire-up the jsx being returned
   // ...field.input says this is an object and i want all the objects properties to be communicated as props to the input tag. saves us from having to do onChange, onFocuse, onBlur, etc.
-  // field.meta.touched - if user has touched the field, show the error otherwise return the string
+  // field.meta.touched - turnary expression. if user has touched the field, then show the error message otherwise return the string
+  // refactored code to destructure field.meta.touched & field.meta.error using ES6 syntax, so you no longer see it below. instead you just see touched & error
   renderField(field) {
+
+    const { meta: { touched, error } } = field;
+    const className= `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
