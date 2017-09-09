3cee84e414aaf178228f3d10409b9b5f6d4ba8db
Andrea Coaker <andicoaker@gmail.com>
2017-09-09 12:17:16 -0400

    rendering posts to posts_index page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -10,16 +11,37 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
-  render () {
-    return (
-      <div>
-        Posts Index
-        </div>
-    );
+  // create helper function below to map over lists of posts and create one <li> for every post we fetch. this is an object that has a list of posts, not an array, so we have to use the lodash map method and not the typical map method on an array.
+  // second argument returns the jsx
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );  
+    });
   }
 
+  render () {
+    // console.log(this.props.posts);
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
 }
 
-// first argument set to null because we're not passing in map state to props.
+function mapStateToProps (state) {
+  return { posts: state.posts };
+}
+
+// Starting off, our first argument set to null because we're not passing in map state to props.
 // code below is just like using the map dispatch to props function used in previous projects
-export default connect (null, { fetchPosts })(PostsIndex);
+// Added in mapStateToProps function above and then added mapStateToProps as first argument below once fucntion was created.
+
+export default connect (mapStateToProps, { fetchPosts })(PostsIndex);
