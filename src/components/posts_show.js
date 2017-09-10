7924781c72e08a5531f10411510cb72fb6f050b0
Andrea Coaker <andicoaker@gmail.com>
2017-09-10 15:03:29 -0400

    added back button to posts_show

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   // accessing the id off of the route from the src/index.js file
@@ -21,6 +22,9 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to="/" className="btn btn-primary">Back to Index</Link>
+        </div>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
